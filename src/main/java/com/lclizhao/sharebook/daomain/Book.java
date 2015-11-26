71d686719ebf0afdac47f4345be19898aaccc5b6
lizhaoz <563868273@qq.com>
2015-11-26 15:20:03 +0800

    第二次测试

---
+++ src/main/java/com/lclizhao/sharebook/daomain/Book.java
@@ -0,0 +1,13 @@
+package com.lclizhao.sharebook.daomain;/**
+ * Created by lizhaoz on 2015/11/26.
+ */
+
+/**
+ * @Name:
+ * @Author: lizhao（作者）
+ * @Version: V1.00 （版本号）
+ * @Create Date: 2015-11-26（创建日期）
+ * @Description:
+ */
+public class Book {
+}
